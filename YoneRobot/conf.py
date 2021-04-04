b88f8f7779bbe490c1100dabfdadd768dc7218b2
kushal-modi <cuteromantickittu@gmail.com>
2021-04-04 20:18:53 +0530

    music

---
+++ YoneRobot/conf.py
@@ -0,0 +1,13 @@
+def get_str_key(name, required=False):
+    if name in DEFAULTS:
+        default = DEFAULTS[name]
+    else:
+        default = None
+    if not (data := env.str(name, default=default)) and not required:
+        log.warn("No str key: " + name)
+        return None
+    elif not data:
+        log.critical("No str key: " + name)
+        sys.exit(2)
+    else:
+        return data
