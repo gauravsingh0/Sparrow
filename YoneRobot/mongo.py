5c725e264189acf9d3bfa306101d83d03135c3dd
kushal-modi <cuteromantickittu@gmail.com>
2021-04-13 04:25:55 +0530

    Update mongo.py

--- YoneRobot/mongo.py
+++ YoneRobot/mongo.py
@@ -9,11 +9,11 @@ from pymongo.errors import ServerSelectionTimeoutError
 from YoneRobot.conf import get_int_key, get_str_key
 
 
-
+MONGO_PORT = get_int_key("27017")
 MONGO_DB_URI = get_str_key("MONGO_DB_URI")
 
 client = MongoClient()
-client = MongoClient(MONGO_DB_URI)
+client = MongoClient(MONGO_DB_URI, MONGO_PORT)
 # motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI)
 db = client["yonerobot"]
 # db = motor[MONGO_DB_URI]
