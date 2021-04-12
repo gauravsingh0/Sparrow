ba2d218ce520e6ae9fe0310d3fd0dec174ca1a33
kushal-modi <cuteromantickittu@gmail.com>
2021-04-13 02:55:14 +0530

    Ai Added

--- YoneRobot/modules/__aifunctions.py
+++ YoneRobot/modules/__aifunctions.py
@@ -29,7 +29,7 @@ async def hmm(event):
   lodu = response.json()
   result = (lodu['message']['text'])
   if "no no" in result:
-   pro = "I am fairly yound and I was made by @Mr_Dark_Prince."
+   pro = "I am fairly yound and I was made by @A_viyu."
    try:
       async with telethn.action(event.chat_id, 'typing'):
            await asyncio.sleep(2)
---
+++ YoneRobot/modules/helper_funcs/aichat.py
@@ -0,0 +1,22 @@
+from YoneRobot import MONGO_DB_URI as db_x
+
+lydia = db_x["CAHTBOT"]
+talkmode = db_x["TALKMODE"]
+
+
+def add_chat(chat_id):
+    stark = lydia.find_one({"chat_id": chat_id})
+    if stark:
+        return False
+    else:
+        lydia.insert_one({"chat_id": chat_id})
+        return True
+
+
+def remove_chat(chat_id):
+    stark = lydia.find_one({"chat_id": chat_id})
+    if not stark:
+        return False
+    else:
+        lydia.delete_one({"chat_id": chat_id})
+        return True
