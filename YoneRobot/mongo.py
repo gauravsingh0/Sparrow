9572109a94632ac6cb4fd5686e565c4fd6f5b246
kushal-modi <cuteromantickittu@gmail.com>
2021-04-13 03:44:25 +0530

    Update mongo.py

--- YoneRobot/mongo.py
+++ YoneRobot/mongo.py
@@ -2,7 +2,7 @@
 import asyncio
 import sys
 
-from motor import motor_asyncio
+# from motor import motor_asyncio
 from YoneRobot import MONGO_DB_URI 
 from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
@@ -14,10 +14,10 @@ MONGO_DB_URI = get_str_key("MONGO_DB_URI")
 
 client = MongoClient()
 client = MongoClient(MONGO_DB_URI)
-motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI)
+# motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI)
 db = client["yonerobot"]
-db = motor[MONGO_DB_URI]
-try:
-    asyncio.get_event_loop().run_until_complete(motor.server_info())
-except ServerSelectionTimeoutError:
-    sys.exit(log.critical("Can't connect to mongodb! Exiting..."))
+# db = motor[MONGO_DB_URI]
+# try:
+#     asyncio.get_event_loop().run_until_complete(motor.server_info())
+# except ServerSelectionTimeoutError:
+#     sys.exit(log.critical("Can't connect to mongodb! Exiting..."))
