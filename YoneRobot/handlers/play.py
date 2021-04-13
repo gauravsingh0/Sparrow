54e333cff54574c3b7c67edb3ea1fced8970ea98
edguru <72596185+edguru@users.noreply.github.com>
2021-04-13 15:48:58 +0530

    Update play.py

--- YoneRobot/handlers/play.py
+++ YoneRobot/handlers/play.py
@@ -1,6 +1,6 @@
 from os import path
 
-from pyrogram import Client
+from YoneRobot import pbot
 from pyrogram.types import Message
 
 from ..callsmusic import callsmusic
@@ -14,7 +14,7 @@ from ..helpers.gets import get_url, get_file_name
 from .. import queues
 
 
-@Client.on_message(command("play") & other_filters)
+@pbot.on_message(command("play") & other_filters)
 @errors
 async def play(_, message: Message):
     audio = (message.reply_to_message.audio or message.reply_to_message.voice) if message.reply_to_message else None
