d1d12c258d4d29f8784fff65dceb8bc2d4397be5
edguru <72596185+edguru@users.noreply.github.com>
2021-04-13 15:40:52 +0530

    Update youtube.py

--- YoneRobot/downloaders/youtube.py
+++ YoneRobot/downloaders/youtube.py
@@ -2,7 +2,7 @@ from os import path
 
 from youtube_dl import YoutubeDL
 
-from ..config import DURATION_LIMIT
+
 from ..helpers.errors import DurationLimitError
 
 ytdl = YoutubeDL(
@@ -19,7 +19,7 @@ def download(url: str) -> str:
     info = ytdl.extract_info(url, False)
     duration = round(info["duration"] / 60)
 
-    if duration > DURATION_LIMIT:
+    if duration > 10:
         raise DurationLimitError(
             f"Videos longer than {DURATION_LIMIT} minute(s) aren’t allowed, the provided video is {duration} minute(s)"
         )
