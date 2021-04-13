6773f03b260ab42e214725f3f583169fc2ee10e4
edguru <72596185+edguru@users.noreply.github.com>
2021-04-13 15:43:52 +0530

    Update __init__.py

--- YoneRobot/__init__.py
+++ YoneRobot/__init__.py
@@ -191,7 +191,7 @@ else:
 
 updater = tg.Updater(TOKEN, workers=WORKERS, use_context=True)
 telethn = TelegramClient("yone", API_ID, API_HASH)
-pbot = Client("yonepbot", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
+pbot = Client("yonepbot", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN , plugins=dict(root= "YoneRobot.handlers"))
 dispatcher = updater.dispatcher
 
 DRAGONS = list(DRAGONS) + list(DEV_USERS)
