8420d33712ceaba57f08fe97051459c13d58c9cb
edguru <72596185+edguru@users.noreply.github.com>
2021-04-15 17:16:11 +0530

    Update mongo.py

--- YoneRobot/mongo.py
+++ YoneRobot/mongo.py
@@ -6,13 +6,17 @@ from motor import motor_asyncio
 from YoneRobot import MONGO_DB_URI 
 from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
+from YoneRobot.conf import get_int_key, get_str_key
 
 
-
-
+MONGO_PORT = get_int_key("27017")
+MONGO_DB_URI = get_str_key("MONGO_DB_URI")
 MONGO_DB = "DaisyX"
-client = MongoClient(MONGO_DB_URI)[MONGO_DB]
-motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI)
+
+
+client = MongoClient()
+client = MongoClient(MONGO_DB_URI, MONGO_PORT)[MONGO_DB]
+motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI, MONGO_PORT)
 db = motor[MONGO_DB]
 db = client["yonerobot"]
 try:
