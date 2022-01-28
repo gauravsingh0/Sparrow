738c709496ced18aa2903a78207f8d8943e460e9
Gaurav singh <84321170+gauravsingh0@users.noreply.github.com>
2022-01-29 04:06:50 +0530

    Update sample_config.py

--- YoneRobot/sample_config.py
+++ YoneRobot/sample_config.py
@@ -4,7 +4,7 @@ import os
 
 
 def get_user_list(config, key):
-    with open("{}/YoneRobot/{}".format(os.getcwd(), config), "r") as json_file:
+    with open("{}/SparrowRobot/{}".format(os.getcwd(), config), "r") as json_file:
         return json.load(json_file)[key]
 
 
@@ -18,8 +18,8 @@ class Config(object):
     API_HASH = "awoo"
     TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
     OWNER_ID = 792109647  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "Kittu"
-    SUPPORT_CHAT = "Kora_Support"  # Your own group for support, do not add the @
+    OWNER_USERNAME = "grbfounder"
+    SUPPORT_CHAT = "elena_support_group"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
         -1001253661229
     )  # Prints any new group the bot is added to, prints just the name and ID.
